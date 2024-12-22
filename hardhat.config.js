4e8fb25f5da1de8c924b1937afed3b9b14a3375f
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2024-12-23 01:06:26 +0530

    [Johny] Update the hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,10 @@
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-deploy")
 module.exports = {
   solidity: "0.8.19",
-};
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+}
